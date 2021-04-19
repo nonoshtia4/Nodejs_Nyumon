7e4cc10c61f58845687ce5a5d11e0cb183267299
nonoshtia4 <nonosshita4@gmail.com>
2021-04-19 16:51:48 +0900

    Add temp.html

--- sample.js
+++ sample.js
@@ -1,9 +1,8 @@
 var http = require('http');
 var fs = require('fs');
-
 var server = http.createServer(function(req, res) {
-  fs.readFile('./temp.html', 'utf-8', function(err, data){
-    res.write(200, {'Content-Type': 'text/html'});
+  fs.readFile('./temp.html', 'utf-8', function(err, data) {
+    res.writeHead(200, {'Content-Type':'text/html'});
     res.write(data);
     res.end();
   });
